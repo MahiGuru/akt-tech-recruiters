564083be3c0812f12a3b1ad6d056fc9ea176c603
mahipal.guru <mahi6535@gmail.com>
2025-06-13 17:02:11 +0530

    removed seedjob button

--- app/(client)/jobs/page.js
+++ app/(client)/jobs/page.js
@@ -90,7 +90,7 @@ export default function Jobs() {
     <div className="min-h-screen bg-gray-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         {/* Seed Jobs Button - Remove this in production */}
-        <SeedJobsButton />
+        {/* <SeedJobsButton /> */}
 
         {/* Header */}
         <div className="mb-8">
